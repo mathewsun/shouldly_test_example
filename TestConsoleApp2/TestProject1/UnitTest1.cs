0d05ff0ad22d3bf790e738c1ea81f70b4cc3ad39
Matthew <mathew.dubrovin@gmail.com>
2022-10-10 16:10:51 +0300

    first test

--- TestConsoleApp2/TestProject1/UnitTest1.cs
+++ TestConsoleApp2/TestProject1/UnitTest1.cs
@@ -5,5 +5,15 @@ namespace TestProject1;
 
 public class UnitTest1
 {
-    
+    [Fact]
+    public void Test9991()
+    {
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        Program.TestWorks().ShouldBe("Works");
+    }
+
 }
