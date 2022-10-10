ee4efe00b46e91fe26f9711fe911bfee092b8ac9
Matthew <mathew.dubrovin@gmail.com>
2022-10-10 13:20:23 +0300

    init commit

---
+++ TestConsoleApp2/TestConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TestConsoleApp2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static string TestWorks()
+        {
+            return "Works";
+        }
+    }
+}
+
+
+public class Person
+{
+    public Person()
+    {
+    }
+
+    public Person(string name)
+    {
+        Name = name;
+        throw (new ArgumentNullException());
+    }
+
+    public string Name { get; set; }
+    public int Salary { get; set; }
+}
